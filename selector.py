d232a08953043af2d10a3022c60472fc8aa06ce2
dave90 <ddfusca@gmail.com>
2017-11-29 12:35:33 +0100

    Add model SVMC and Normalizer

--- selector.py
+++ selector.py
@@ -6,6 +6,9 @@ import os
 import sklearn.tree
 import sklearn.neighbors.typedefs
 import sklearn.tree._utils
+import sklearn.svm
+import numpy as np
+from sklearn.preprocessing import Normalizer
 
 def get_script_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
@@ -13,6 +16,9 @@ def get_script_path():
 
 dir=get_script_path()
 clf = joblib.load(os.path.join(dir,"selector_model.pkl"))
+
+norm = Normalizer()
+norm = joblib.load(os.path.join(dir,"norm_classes.pkl"))
 
 row=""
 for line in fileinput.input():
@@ -22,6 +28,11 @@ row=row.strip()
 data1=row.split(" ")
 data=[]
 data.append(data1)
+data = [ [float(s) for s in d] for d in data] 
+data = norm.transform(data)
 
 res=clf.predict(data)
-print(res)
+if len(res)>0:
+	print([res[0].lower()])
+else:
+	print(res.lower())
